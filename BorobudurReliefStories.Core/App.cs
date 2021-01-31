a94dc579a051946f92c67714bcab6c1ffb32a7cc
Partho P. Das <parthopdas@live.com>
2021-01-31 11:27:31 +1000

    Fixing build break

--- BorobudurReliefStories.Core/App.cs
+++ BorobudurReliefStories.Core/App.cs
@@ -1,9 +1,8 @@
-using MvvmCross.IoC;
-using MvvmCross.ViewModels;
-using BorobudurReliefStories.Core.ViewModels.Home;
-
 namespace BorobudurReliefStories.Core
 {
+    using MvvmCross.ViewModels;
+    using BorobudurReliefStories.Core.ViewModels.Home;
+
     public class App : MvxApplication
     {
         public override void Initialize()
